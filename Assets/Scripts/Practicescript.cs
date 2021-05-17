caae71a50cfd6c78e414464ddd7365a0560ce69a
dbraunlich <david.johannes.b@gmail.com>
2021-05-17 13:46:22 +0200

    Nummer 2

--- Assets/Scripts/Practicescript.cs
+++ Assets/Scripts/Practicescript.cs
@@ -25,8 +25,19 @@ public class Practicescript : MonoBehaviour
         arrayColoredImagesRed
         = GameObject.FindGameObjectsWithTag("red");
 
+        Debug.Log("Element 0 von array1 hat den Wert: " +array1[0]);
+        Debug.Log("Element 0 von array2 hat den Wert: " + array2[0]);
+        Debug.Log("Element 0 von array3 hat den Wert: " + array3[0]);
+        Debug.Log("Element 0 von array4 hat den Wert: " + array4[0]);
+        Debug.Log("Name des ersten GameObjects: " + arrayColoredImagesRed[0].name);
 
-  
+
+        Debug.Log("Das letzte Element von array1 hat den Wert: " + array1[array1.Length - 1]);
+        Debug.Log("Das letzte Element von array2 hat den Wert: " + array2[array2.Length - 1]);
+        Debug.Log("Das letzte Element von array3 hat den Wert: " + array3[array3.Length - 1]);
+        Debug.Log("Das letzte Element von array4 hat den Wert: " + array4[array4.Length - 1]);
+        Debug.Log("Name des ersten GameObjects: " + arrayColoredImagesRed[0].name);
+        6
 
 
     }
